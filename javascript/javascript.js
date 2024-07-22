587396d20bc4365b8be94ce0205d7650096aec58
Oviya-R <oviya.ravichandran2004@gmail.com>
2024-07-22 17:21:36 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,14 @@
+
+function red1(){
+    document.getElementsByClassName("red1")[0].style.backgroundColor="red";
+}
+function red2(){
+    document.getElementsByClassName("red2")[0].style.backgroundColor="red";
+}
+function red3(){
+    document.getElementsByClassName("red3")[0].style.backgroundColor="red";
+}
+
+function green(){
+    document.getElementsByClassName("green")[0].style.backgroundColor="green";
+}
